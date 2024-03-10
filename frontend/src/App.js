ccbcde1f7c116908886a827ba43b767892dfaa90
sudhanshu8352 <82777068+sudhanshu8352@users.noreply.github.com>
2024-03-11 00:41:05 +0530

    UI Changes done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,21 @@
-import './App.css';
-import { useSession, useSupabaseClient, useSessionContext } from '@supabase/auth-helpers-react';
-import DateTimePicker from 'react-datetime-picker';
-import { useEffect, useState } from 'react';
+import "./App.css";
+import {
+  useSession,
+  useSupabaseClient,
+  useSessionContext,
+} from "@supabase/auth-helpers-react";
+import DateTimePicker from "react-datetime-picker";
+import { useEffect, useState } from "react";
 import "react-datetime-picker/dist/DateTimePicker.css";
 import Calendar from "react-calendar";
-import "react-calendar/dist/Calendar.css";
+// import "react-calendar/dist/Calendar.css";
 
 function App() {
-  const [ start, setStart ] = useState(new Date());
-  const [ end, setEnd ] = useState(new Date());
-  const [ eventName, setEventName ] = useState("");
-  const [ eventDescription, setEventDescription ] = useState("");
+  const [start, setStart] = useState(new Date());
+  const [end, setEnd] = useState(new Date());
+  const [eventName, setEventName] = useState("");
+  const [eventDescription, setEventDescription] = useState("");
+  const [isAddActivityOpen, setIsAddActivityOpen] = useState(false); // New state
 
   const session = useSession(); // tokens, when session exists we have a user
   const supabase = useSupabaseClient(); // talk to supabase!
@@ -38,7 +43,7 @@ function App() {
         0
       ).toISOString();
 
-      console.log("Time: ", timeMin, timeMax);
+      // console.log("Time: ", timeMin, timeMax);
 
       try {
         const response = await fetch(
@@ -46,13 +51,13 @@ function App() {
           {
             method: "GET",
             headers: {
-              'Authorization':'Bearer ' + session.provider_token // Access token for google
+              Authorization: "Bearer " + session.provider_token, // Access token for google
             },
           }
         );
 
         const data = await response.json();
-        console.log("Data: ", data);
+        // console.log("Data: ", data);
         setEvents(data.items || []);
       } catch (error) {
         console.error("Error fetching events:", error);
@@ -61,19 +66,19 @@ function App() {
 
     fetchEvents();
   }, [date]);
-  
-  if(isLoading) {
-    return <></>
+
+  if (isLoading) {
+    return <></>;
   }
 
   async function googleSignIn() {
     const { error } = await supabase.auth.signInWithOAuth({
-      provider: 'google',
+      provider: "google",
       options: {
-        scopes: 'https://www.googleapis.com/auth/calendar'
-      }
+        scopes: "https://www.googleapis.com/auth/calendar",
+      },
     });
-    if(error) {
+    if (error) {
       alert("Error logging in to Google provider with Supabase");
       console.log(error);
     }
@@ -86,88 +91,176 @@ function App() {
   async function createCalendarEvent() {
     console.log("Creating calendar event");
     const event = {
-      'summary': eventName,
-      'description': eventDescription,
-      'start': {
-        'dateTime': start.toISOString(), // Date.toISOString() ->
-        'timeZone': Intl.DateTimeFormat().resolvedOptions().timeZone // America/Los_Angeles
+      summary: eventName,
+      description: eventDescription,
+      start: {
+        dateTime: start.toISOString(), // Date.toISOString() ->
+        timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone, // America/Los_Angeles
       },
-      'end': {
-        'dateTime': end.toISOString(), // Date.toISOString() ->
-        'timeZone': Intl.DateTimeFormat().resolvedOptions().timeZone // America/Los_Angeles
+      end: {
+        dateTime: end.toISOString(), // Date.toISOString() ->
+        timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone, // America/Los_Angeles
+      },
+    };
+    await fetch(
+      "https://www.googleapis.com/calendar/v3/calendars/primary/events",
+      {
+        method: "POST",
+        headers: {
+          Authorization: "Bearer " + session.provider_token, // Access token for google
+        },
+        body: JSON.stringify(event),
       }
-    }
-    await fetch("https://www.googleapis.com/calendar/v3/calendars/primary/events", {
-      method: "POST",
-      headers: {
-        'Authorization':'Bearer ' + session.provider_token // Access token for google
-      },
-      body: JSON.stringify(event)
-    }).then((data) => {
-      return data.json();
-    }).then((data) => {
-      console.log(data);
-      alert("Event created, check your Google Calendar!");
-    });
+    )
+      .then((data) => {
+        return data.json();
+      })
+      .then((data) => {
+        console.log(data);
+        alert("Event created, check your Google Calendar!");
+        setIsAddActivityOpen(false);
+      });
   }
-
 
   return (
     <div className="App">
-    <div style={{ width: "100%" }}>
-      {session ? (
-        <>
-          <nav
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-              padding: "10px",
-              backgroundColor: "#eee",
-            }}
-          >
-            <div>
-              {session
-                ? `Logged in as: ${session.user.email}`
-                : "Not logged in"}
+      <div style={{ width: "100%" }}>
+        {session ? (
+          <>
+            <nav
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                padding: "30px 50px",
+                backgroundColor: "#000",
+                color: "#fffff",
+              }}
+            >
+              <div style={{
+                color: "white",
+                fontWeight: "bold",
+                fontSize: "18px"
+              }}>{session ? session.user.email : "Not logged in"}</div>
+              {session && (
+                <button className="sign-out-btn" onClick={signOut}>
+                  Sign Out
+                </button>
+              )}
+            </nav>
+            <h1
+              style={{
+                textAlign: "center",
+                alignItems: "center",
+              }}
+            >
+              Activity Tracker
+            </h1>
+            <div className="main-body">
+              <div className="calender-body">
+                <Calendar onChange={setDate} value={date} />
+              </div>
+              <div className="activity-wrap">
+                <div className="activity-list">
+                  <h3
+                    style={{
+                      marginLeft: "10px",
+                    }}
+                  >
+                    Activities on {date.toDateString()}
+                  </h3>
+                  <ul>
+                    {events.map((event) => (
+                      <li key={event.id}>
+                        <strong>{event.summary}</strong>
+                        <br />
+                        {event.description}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
             </div>
-            {session && <button onClick={signOut}>Sign Out</button>}
-          </nav>
-          <div>
-            <Calendar onChange={setDate} value={date} />
-          </div>
-          <div>
-            <h3>Events on {date.toDateString()}</h3>
-            <ul>
-              {events.map((event) => (
-                <li key={event.id}>{event.summary}</li>
-              ))}
-            </ul>
-          </div>
-          {/* <button onClick={openAddActivityPopup}>Add Activity</button>
-          {isAddActivityOpen && (
-            <AddActivity onClose={closeAddActivityPopup} />
-          )} */}
-          <h2>Hey there {session.user.email}</h2>
-          <p>Start of your event</p>
-          <DateTimePicker onChange={setStart} value={start} />
-          <p>End of your event</p>
-          <DateTimePicker onChange={setEnd} value={end} />
-          <p>Event name</p>
-          <input type="text" onChange={(e) => setEventName(e.target.value)} />
-          <p>Event description</p>
-          <input type="text" onChange={(e) => setEventDescription(e.target.value)} />
-          <hr />
-          <button onClick={() => createCalendarEvent()}>Create Calendar Event</button>
-          <p></p>
-          <button onClick={() => signOut()}>Sign Out</button>
-        </>
-      ) : (
-        <>
-          <button onClick={() => googleSignIn()}>Sign In With Google</button>
-        </>
-      )}
+
+            <div className="activity-btn-div">
+              <button
+                className="add-activity-button"
+                onClick={() => setIsAddActivityOpen(!isAddActivityOpen)}
+              >
+                Add Activity
+              </button>
+            </div>
+            {/* <hr /> */}
+            {isAddActivityOpen && (
+              <>
+                <div className="add-activity">
+                  <h2
+                    style={{
+                      textAlign: "center",
+                      alignItems: "center",
+                    }}
+                  >
+                    Add an event
+                  </h2>
+                  <div className="inputs">
+                    <label>Start of your activity:</label>
+                    <DateTimePicker onChange={setStart} value={start} />
+                  </div>
+                  <div className="inputs">
+                    <label>End of youractivity:t</label>
+                    <DateTimePicker onChange={setEnd} value={end} />
+                  </div>
+                  <div className="inputs">
+                    <label>Activity name</label>
+                    <input
+                      className="text-input"
+                      type="text"
+                      onChange={(e) => setEventName(e.target.value)}
+                    />
+                  </div>
+                  <div className="inputs">
+                    <label>Activity description</label>
+                    <input
+                      className="text-input"
+                      type="textarea"
+                      onChange={(e) => setEventDescription(e.target.value)}
+                    />
+                  </div>
+                  <hr />
+                  <div className="submit-activity">
+                    <button
+                      className="submit-activity-btn"
+                      onClick={() => createCalendarEvent()}
+                    >
+                      Create Calendar Activity
+                    </button>
+                  </div>
+                  <p></p>
+                </div>
+              </>
+            )}
+          </>
+        ) : (
+          <>
+            <button onClick={() => googleSignIn()}>Sign In With Google</button>
+          </>
+        )}
+        <div
+          style={{
+            marginTop: "20px",
+            backgroundColor: "#000",
+            color: "#fff",
+            padding: "10px 0px",
+            textAlign: "center",
+            position: "fixed",
+            bottom: "0",
+            width: "100%",
+          }}
+        >
+          @Sudhanshu_Shekhar
+        </div>
+      </div>
     </div>
-  </div>
   );
 }
 
